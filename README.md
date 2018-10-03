67258a4868ff7a1c9f80a783660358c939721f5d
Arvid E. Picciani <aep@exys.org>
2018-10-03 15:36:46 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+pug (jade) templates
+---------------------
+
+reimplemented in rust for performance reasons.
+
+| pug.js | pug-rs |
+|--------|--------|
+| 780ms  | 29ms   |
+
+
+usage:
+-------
+
+```
+$ cargo install pug
+$ pug < thing.pug > thing.html
+```
---
+++ src/main.rs
@@ -0,0 +1,148 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+use pest::error::Error;
+use std::io::{self, Read};
+
+#[derive(Parser)]
+#[grammar = "pug.pest"]
+pub struct PugParser;
+
+
+fn parse(mut file: String) -> Result<String, Error<Rule>> {
+    file.push('\n');
+    file.push('\n');
+    let file = PugParser::parse(Rule::file, &file)?.next().unwrap().into_inner();
+
+
+    let mut html = String::new();
+
+    let mut indent   = 0;
+    let mut tagstack  : Vec<(usize, String)>  = Vec::new();
+
+    for decl in file {
+        match decl.as_rule() {
+            Rule::indent => {
+                indent = decl.as_str().len();
+
+                while let Some((ind, element)) = tagstack.last().cloned() {
+                    if ind >= indent {
+                        html.push_str("</");
+                        html.push_str(&element);
+                        html.push_str(">");
+                        tagstack.pop();
+                    } else {
+                        break
+                    }
+                }
+            },
+            Rule::tag => {
+                let mut element = "div".to_string();
+                let mut id      = None;
+                let mut class   = Vec::new();
+                let mut attrs   = Vec::new();
+                for e in decl.into_inner() {
+                    match e.as_rule() {
+                        Rule::element => {
+                            element = e.as_str().to_string();
+                        },
+                        Rule::class => {
+                            class.push(e.into_inner().next().unwrap().as_str().to_string());
+                        },
+                        Rule::id => {
+                            id = Some(e.into_inner().next().unwrap().as_str().to_string());
+                        },
+                        Rule::attrs => {
+                            for e in e.into_inner() {
+                                let mut e = e.into_inner();
+                                let key   = e.next().unwrap().as_str();
+                                let value = e.next().unwrap().as_str();
+                                attrs.push(format!("{}={}", key,value));
+                            }
+                        },
+                        _ => unreachable!(),
+                    }
+                }
+
+                html.push('<');
+                html.push_str(&element);
+                if class.len() > 0 {
+                    html.push_str(" class=\"");
+                    html.push_str(&class.join(" "));
+                    html.push('"');
+                }
+                if let Some(id) = id {
+                    html.push_str(" id=\"");
+                    html.push_str(&id);
+                    html.push('"');
+                }
+                for attr in attrs {
+                    html.push(' ');
+                    html.push_str(&attr);
+                }
+                html.push('>');
+                tagstack.push((indent, element));
+            },
+            Rule::comment => {
+            },
+            Rule::text => {
+                html.push_str(decl.as_str());
+            },
+            Rule::EOI => {
+            },
+            any => panic!(println!("parser bug. did not expect: {:?}", any)),
+        }
+    }
+
+    Ok(html)
+}
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    let html = parse(buffer).unwrap();
+    println!("{}", html);
+}
+
+
+#[test]
+pub fn eoi() {
+
+    let html = parse(
+r#"body#blorp.herp.derp
+  a(href="google.de")
+derp
+  yorlo jaja"#
+  .to_string()).unwrap();
+    assert_eq!(html,
+    r#"<body class="herp derp" id="blorp"><a href="google.de"></a></body><derp><yorlo>jaja</yorlo></derp>"#
+    );
+
+
+    let html = parse(
+r#"body#blorp.herp.derp
+  a(href="google.de")
+derp
+  yorlo jaja
+  "#
+  .to_string()).unwrap();
+    assert_eq!(html,
+    r#"<body class="herp derp" id="blorp"><a href="google.de"></a></body><derp><yorlo>jaja</yorlo></derp>"#
+    );
+
+    let html = parse(
+r#"body#blorp.herp.derp
+  a(href="google.de")
+derp
+  yorlo jaja
+
+
+
+"#
+   .to_string()).unwrap();
+    assert_eq!(html,
+    r#"<body class="herp derp" id="blorp"><a href="google.de"></a></body><derp><yorlo>jaja</yorlo></derp>"#
+    );
+}
